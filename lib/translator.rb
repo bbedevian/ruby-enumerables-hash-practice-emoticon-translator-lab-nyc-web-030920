d617dc2618c02dcd126870cb8fdf83fd6b41d704
Brett Bedevian <brettbedevian@gmail.com>
2020-02-26 15:49:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,16 +10,16 @@ result = {
 }
 
 emoticons.each do |key, value| 
-  result['get_meaning'][value[1]] = key 
-  result['get_emoticon'][value[0]] = emoticons[key][1] 
+  result['get_meaning'][value[1]] = key #japanese key, meaning value 
+  result['get_emoticon'][value[0]] = emoticons[key][1] #english key, japanese value
   end 
   
   result
 end
 
-def get_japanese_emoticon(file_path, emoticon)
+def get_japanese_emoticon(file_path, emoticon) #takes english emote, return japanese
 
-load_library(file_path)
+# load_library(file_path)
 
 
 end
