9832242a97cd7c73031ca4c7d158276d2a02292e
Brett Bedevian <brettbedevian@gmail.com>
2020-02-26 15:36:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,10 @@ result = {
   'get_emoticon' => {} 
 }
 
-empticons.each do |key, value| #key is the meaning, values are the emotes [1 is japanese][0 is english]
-  result['get_meaning'][value[1]] = key 
-  result['get_emotion'][value[0]] = key 
-  end 
+# empticons.each do |key, value| #key is the meaning, values are the emotes [1 is japanese][0 is english]
+#   result['get_meaning'][value[1]] = key 
+#   result['get_emotion'][value[0]] = key 
+#   end 
 end
 
 def get_japanese_emoticon
